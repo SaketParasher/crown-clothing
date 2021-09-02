80900ff95829b1bf97f6fdbced8fd8684826928d
Saket Saurav <saket.saurav@publicissapient.com>
2021-09-02 13:32:44 +0530

    adding Header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import Homepage from './Pages/Homepage/Homepage';
 import Shoppage from './Pages/Shop/Shop';
 import { Route, Switch } from 'react-router-dom';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop' component={Shoppage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={Shoppage} />
+      </Switch>
+    </div>
   );
 }
 
